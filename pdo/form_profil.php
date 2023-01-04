d94f4b6354b95fc9c34654929d540cb6efd7cb56
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-04 15:59:56 +0100

    add header

--- pdo/form_profil.php
+++ pdo/form_profil.php
@@ -27,34 +27,9 @@ if (isset($_POST['update'])) {
     <title>PDO - Profil</title>
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a href="" class="nav-link active"></a>
-                    </li>
-                    <li>
-                        <a href="" class="nav-link active"></a>
-                    </li>
-                    <li>
-                        <?php if (isset($_SESSION['login']) != null) { ?>
-                            <a href="form_profil.php" class="btn btn-warning"><?php echo $_SESSION['login']; ?></a>
-                        <?php } else { ?>
-                            <a href="form_connexion.php" class="btn btn-warning">Profil</a>
-                        <?php } ?>
-                    </li>
-                </ul>
-                <form action="" method="post" class="d-flex">
-                    <?php if (isset($_SESSION['login']) != null) { ?>
-                        <input type="submit" name="deconnexion" value="Se déconnecter"  class="btn btn-danger">
-                    <?php } else { ?>
-                    <a href="form_connexion.php" class="btn btn-success">Connexion</a>
-                    <?php } ?>
-                </form>
-            </div>
-        </nav>
-    </header>
+<!-- <header>-->
+<?php include ('header.php'); ?>
+<!-- <header>-->
     <main>
         <article>
             <section>
---
+++ pdo/header.php
@@ -0,0 +1,31 @@
+    
+    
+    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="../index.php" class="nav-link active">Acceuil class</a>
+                    </li>
+                    <li>
+                        <a href="index.php" class="nav-link active">Acceuil PDO</a>
+                    </li>
+                    <li>
+                        <?php if (isset($_SESSION['login']) != null) { ?>
+                            <a href="form_profil.php" class="btn btn-warning"><?php echo $_SESSION['login']; ?></a>
+                        <?php } else { ?>
+                            <a href="form_connexion.php" class="btn btn-warning">Profil</a>
+                        <?php } ?>
+                    </li>
+                </ul>
+                <form action="" method="post" class="d-flex">
+                    <?php if (isset($_SESSION['login']) != null) { ?>
+                        <input type="submit" name="deconnexion" value="Se déconnecter"  class="btn btn-danger">
+                    <?php } else { ?>
+                    <a href="form_connexion.php" class="btn btn-success">Connexion</a>
+                    <?php } ?>
+                </form>
+            </div>
+        </nav>
+    </header>
