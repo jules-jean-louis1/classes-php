5cb8adec715125d77304344b2b4ac82b485645e9
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-04 09:15:24 +0100

    connexion a la base plesk

--- bdd_connect.php
+++ bdd_connect.php
@@ -5,6 +5,7 @@ $connect = mysqli_connect('localhost', 'root', '', 'classes');
 
 // ajout de la connexion à la base de données pour plesk
 
+//$connect = mysqli_connect('localhost', 'jjl-classes', 'wAr6r6$81', 'jules-jean-louis_classes');
 
 mysqli_set_charset($connect, 'utf8');
 ?>
