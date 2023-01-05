97987d7b6a322222b2096d139c650857149e1e04
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-05 09:46:02 +0100

    add page info

---
+++ pdo/getinfos.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infos - PDO</title>
+</head>
+<body>
+    <!-- <header>-->
+    <?php include ('header.php'); ?>
+    <!-- <header>-->
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>Infos</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p>Voici les infos de l'utilisateur</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <!-- <footer>-->
+    <?php include ('footer.php'); ?>
+    <!-- <footer>-->
+</body>
+</html>
